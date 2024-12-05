1f97673c659309521e38b3bad9470e5815bbc8c0
Mayank Jaiswal <jaiswalmayank450@gmail.com>
2024-12-05 23:31:17 +0530

    step1

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -8,7 +8,8 @@ import { io, Socket } from 'socket.io-client';
 export class ChatService {
   private socket: Socket;
   private messageSubject: Subject<string> = new Subject();
-
+  private connSubject: Subject<string> = new Subject();
+  socketId:any;
   constructor() {
     // Connect to WebSocket server
     // this.socket = io('http://localhost:3000');
@@ -16,23 +17,39 @@ export class ChatService {
       transports: ['websocket'],  // Use WebSocket transport only
       withCredentials: true
     });
-
+    this.socket.on('connect', () => {
+      console.log('Connected with socket ID:', this.socket.id);  // socket.id will give you the ID
+      this.socketId = this.socket.id
+    });
+    this.socket.on('disconnect', () => {
+      console.log('Connected with socket ID:', this.socket.id);  // socket.id will give you the ID
+      this.socketId = ''
+    });
+  }
+  getSocketId(){
+   return this.socketId
   }
 
   // Send a chat message to the server
-  sendMessage(message: string): void {
-    this.socket.emit('chat message', message);
+  sendMessage(message: any): void {
+    this.socket.emit('chat message', {...message,fromId:this.socketId});
   }
 
   // Listen for new chat messages from the server
   listenForMessages() {
-    this.socket.on('chat message', (msg: string) => {
+    this.socket.on('chat message', (msg: any) => {
       this.messageSubject.next(msg);
+    });
+    this.socket.on('newConnection', (msg: any) => {
+      this.connSubject.next(msg);
     });
   }
 
   // Observable to subscribe to new messages
   getMessages() {
     return this.messageSubject.asObservable();
+  }
+  getConn() {
+    return this.connSubject.asObservable();
   }
 }
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -11,25 +11,50 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule,FormsModule]
 })
 export class ChatComponent implements OnInit {
-  message: string = '';
-  messages: string[] = [];
-
+  message: any = '';
+  messages: any[] = [];
+  username='';
+  isUserNameSet=false;
+  mySocketId:any;
+  allUsers:any=[];
   constructor(private chatService: ChatService) { }
 
+  fn(){
+    return !this.username
+  }
   ngOnInit(): void {
+    const storedUsername = sessionStorage.getItem('username');
+    if (storedUsername) {
+      this.username = storedUsername;
+      this.isUserNameSet = true;
+    }
+    this.mySocketId = this.chatService.socketId;
+
     // Start listening for incoming messages
     this.chatService.listenForMessages();
 
     // Subscribe to the message observable
-    this.chatService.getMessages().subscribe((msg) => {
+    this.chatService.getMessages().subscribe((msg:any) => {
       this.messages.push(msg);
     });
+    this.chatService.getConn().subscribe((conArr:any) => {
+      this.allUsers=conArr;
+    });
   }
-
+  getMySocketId(){
+    return this.chatService.getSocketId()
+  }
   sendMessage(): void {
     if (this.message.trim()) {
-      this.chatService.sendMessage(this.message);
+      this.chatService.sendMessage({message:this.message,username:this.username});
       this.message = ''; // Clear the message input
+    }
+  }
+  setUsername(): void {
+    // Store the username in sessionStorage
+    if (this.username.trim()) {
+      sessionStorage.setItem('username', this.username);
+      this.isUserNameSet = true;
     }
   }
 }
